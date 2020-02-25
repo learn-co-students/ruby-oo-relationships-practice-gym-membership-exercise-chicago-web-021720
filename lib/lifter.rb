2567f863fa247775e7e16d0278c0c6c5f4bad233
Ethan Davidson <theejdavidson@gmail.com>
2020-02-25 17:41:28 -0600

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,47 @@
 class Lifter
   attr_reader :name, :lift_total
+  # get a list of all lifters
+  # a list of memberships for a specific lifter
+  # list of all gyms a specific lifter has memberships to
+  # average lift total of all lifters
+  #
+  @@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def join_gym(gym, cost)
+    Membership.new(cost, gym, self)
+  end
+
+  def memberships
+    Membership.all.select do |membership|
+      membership.lifter == self
+    end
+  end
+
+  def gyms
+    self.memberships.map do |membership|
+      membership.gym
+    end
+  end
+
+  def self.average_lift_total
+    totals = self.all.map { |lifter| lifter.lift_total }
+    total = totals.sum 
+    total / self.all.size
+  end
+
+  def total_membership_cost
+    cost_arr = self.memberships.map { |membership| membership.cost }
+    cost_arr.sum
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,32 @@
 require_relative '../config/environment.rb'
 
 # test code goes here
+arnold = Lifter.new("arnold", 500)
+jim = Lifter.new("jim", 200)
+before_guy = Lifter.new("before guy", 2)
+after_guy = Lifter.new("after guy", 1000)
+arnolds_gym = Gym.new("Arnold's Gym")
+la_fitness = Gym.new("LA Fitness")
+
+arnold.join_gym(arnolds_gym, 0)
+arnold.join_gym(la_fitness, 1000)
+jim.join_gym(arnolds_gym, 100)
+before_guy.join_gym(arnolds_gym, 100)
+after_guy.join_gym(arnolds_gym, 100)
+
+p "list of all lifters: #{Lifter.all}"
+p "list of all jim's memberships: #{jim.memberships}"
+p "list of all jim's gyms: #{jim.gyms}"
+p "list all of arnold's gyms: #{arnold.gyms}"
+p "avg lift totals: #{Lifter.average_lift_total}"
+p "arnold's total membership cost: #{arnold.total_membership_cost}"
+p "list of la fitness memberships: #{la_fitness.memberships}"
+p "list of arnolds gym lifters: #{arnolds_gym.lifters}"
+p "list of arnolds gym lifters by name: #{arnolds_gym.lifters_names}"
+p "combined lift total at arnolds gym: #{arnolds_gym.combined_lift_total}"
+
+
+
 
 binding.pry
 
