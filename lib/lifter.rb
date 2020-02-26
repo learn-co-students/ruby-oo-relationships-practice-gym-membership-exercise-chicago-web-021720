6d25c34258a243c1f79587617d899a18dc3b113d
mpottebaum <mpottebaum@gmail.com>
2020-02-25 22:58:12 -0600

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,36 @@
 class Lifter
   attr_reader :name, :lift_total
+  @@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
+  end
+
+  def self.all
+    Membership.all.collect {|ms| ms.lifter}.uniq
+  end
+
+  def self.average_lift_total
+    lift_totals = self.all.collect {|lifter| lifter.lift_total}
+    lift_totals.sum / lift_totals.length.to_f
+  end
+
+  def memberships
+    Membership.all.select {|ms| ms.lifter == self}
+  end
+
+  def gyms
+    self.memberships.collect {|ms| ms.gym}
+  end
+
+  def total_membership_cost
+    all_membership_costs = self.memberships.collect {|ms| ms.cost}
+    all_membership_costs.sum
+  end
+
+  def sign_up_for_gym(gym, cost)
+    Membership.new(cost, self, gym)
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,33 @@ require_relative '../config/environment.rb'
 
 # test code goes here
 
+jerry = Lifter.new("Jerry", 200)
+jack = Lifter.new("Jack", 300)
+jonas = Lifter.new("Jonas", 250)
+jeremiah = Lifter.new("Jeremiah", 269)
+john = Lifter.new("John", 208)
+jonah = Lifter.new("Jonah", 246)
+
+wicker_park = Gym.new("Wicker Park")
+logan_square = Gym.new("Logan Square")
+ukrainian_village = Gym.new("Ukrainian Village")
+
+jerry.sign_up_for_gym(wicker_park, 40)
+jack.sign_up_for_gym(wicker_park, 40)
+jonas.sign_up_for_gym(wicker_park, 40)
+jeremiah.sign_up_for_gym(wicker_park, 40)
+
+john.sign_up_for_gym(logan_square, 50)
+jonah.sign_up_for_gym(logan_square, 50)
+jerry.sign_up_for_gym(logan_square, 50)
+
+jack.sign_up_for_gym(ukrainian_village, 35)
+jonas.sign_up_for_gym(ukrainian_village, 35)
+jeremiah.sign_up_for_gym(ukrainian_village, 35)
+john.sign_up_for_gym(ukrainian_village, 35)
+jonah.sign_up_for_gym(ukrainian_village, 35)
+
+
 binding.pry
 
 puts "Gains!"
